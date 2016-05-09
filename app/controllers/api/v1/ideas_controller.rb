3540ef6da66fb7ff322dbf5082dd1a7e75324b7d
Erinna Chen <erinna.chen@gmail.com>
2016-05-09 15:21:30 -0600

    Add more details to new idea feature test * Check idea is committed to database * Check text fields are clear

--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -5,10 +5,6 @@ class Api::V1::IdeasController < Api::V1::BaseController
     respond_with :api, :v1, Idea.create(idea_params)
   end
 
-  # def show
-  #   respond_with :api, :v1, Idea.find(params[:id])
-  # end
-
   private
 
     def idea_params
