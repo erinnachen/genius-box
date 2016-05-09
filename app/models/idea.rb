533832c1b3b72e328445c4ae20c028a0b12a246c
Erinna Chen <erinna.chen@gmail.com>
2016-05-09 10:50:09 -0600

    Add basic validations on Idea model

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,2 +1,6 @@
 class Idea < ActiveRecord::Base
+  validates :title, presence: true
+  validates :body, presence: true
+
+  enum quality: [:meh, :plausible, :genius] 
 end
--- spec/models/idea_spec.rb
+++ spec/models/idea_spec.rb
@@ -4,4 +4,9 @@ RSpec.describe Idea, type: :model do
   it { should validate_presence_of :title }
   it { should validate_presence_of :body }
   it { should define_enum_for(:quality).with([:meh, :plausible, :genius]) }
+
+  it "should by default have value of meh" do
+    idea = Idea.new(title: "New idea", body: "Another thought")
+    expect(idea.quality).to eq "meh"
+  end
 end
