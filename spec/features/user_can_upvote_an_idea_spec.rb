fb306ee7abe82eea5cc0d1b60a511f08bbba63c5
Erinna Chen <erinna.chen@gmail.com>
2016-05-10 14:35:45 -0600

    Check if quality stays at genius when upvoted

--- spec/features/user_can_upvote_an_idea_spec.rb
+++ spec/features/user_can_upvote_an_idea_spec.rb
@@ -24,4 +24,18 @@ RSpec.feature "user can upvote an idea", js: true do
     end
   end
 
+  context "quality is 2" do
+    scenario "quality stays at 2" do
+      idea = create(:idea, quality: 2)
+      visit '/'
+
+      first(".upvote").click
+      sleep 1
+
+      within(".idea") do
+        expect(page).to have_content "genius"
+      end
+    end
+  end
+
 end
