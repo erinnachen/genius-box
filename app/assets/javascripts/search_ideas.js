ea4b0cff1953888b2e9df65e6e57dab39cc52c71
Erinna Chen <erinna.chen@gmail.com>
2016-05-11 22:23:34 -0600

    Change handling of enter in search bar

--- app/assets/javascripts/search_ideas.js
+++ app/assets/javascripts/search_ideas.js
@@ -1,10 +1,12 @@
 $(document).ready(function(){
-  $( "#idea_search" ).keyup(function(e) {
+  $( "#idea_search" ).keydown(function(e){
     if (e.keyCode === 13) {
       e.preventDefault();
       return;
     }
+  })
 
+  $( "#idea_search" ).keyup(function(e) {
     $('.idea').each(function(){
       var searchStr = $('#idea_search').val().toLowerCase();
       var title = this.children[0].children[0].innerHTML.toLowerCase();
